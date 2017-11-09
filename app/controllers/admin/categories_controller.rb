9be8cbccb06e398ada3086e370a6ec4e9ddf1d13
Mel <zzidante@gmail.com>
2017-11-09 02:15:12 +0000

    Creating new category in admin takes user to a list of All Categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -12,7 +12,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :products], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
