84da8527bcb3fcbc8c5baefc40cec655f8160f6a
Mel <zzidante@gmail.com>
2017-11-10 07:47:05 +0000

    Email now displays product details from order

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def confirmed_purchase(order)
     @order = order
-    mail(to: @order.email, subject: @order.id)
+    mail(to: @order.email, subject: order.id)
   end
 
 end
