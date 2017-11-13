61926f05250e071e51aea7cb6554ac85033566f9
Mel <zzidante@gmail.com>
2017-11-13 01:44:01 +0000

    Reviews now post

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @reviews = @product.reviews.create(review_params)
+    redirect_to '/'
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
